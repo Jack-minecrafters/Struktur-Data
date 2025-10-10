2408bef49c42057725dcca52e9d7c3f1931cf847
Jack-minecrafters <240705107@student.ar-raniry.ac.id>
2025-10-10 13:48:55 +0700

    TAMMBAHAN

--- BelajarQueue.py
+++ BelajarQueue.py
@@ -1,38 +1,16 @@
-class Queue:
-    def __init__(self):
-        self.qlist = []
+from collections import deque
 
-    def isEmpty(self):
-        return len(self) == 0
-    
-    def __len__(self):
-        return len(self.qlist)
-    
-    def enqueue(self, data):
-        self.qlist.append(data)
+antrian = deque()
 
-    def dequeue(self):
-        if self.isEmpty():
-            print('Queue kosong. Tidak ada data yang dapat di-dequeue.')
-            return None
-        else:
-            return self.qlist.pop(0)
+# Customer datang
+antrian.append("Ali")
+antrian.append("Budi")
+antrian.append("Citra")
 
-    def peek(self):
-        if self.isEmpty():
-            print('Queue kosong. Tidak ada data yang dapat diintip.')
-            return None
-        else:
-            return self.qlist[0]
-    
-    def display(self):
-        if self.isEmpty():
-            print('Queue kosong.')
-        else:
-            for item in self.qlist:
-                print(item, end=' ')
-            print()  # baris baru
-    
-    def delete_all(self):
-        while not self.isEmpty():
-            self.dequeue()
+print("Antrian saat ini:", list(antrian))
+
+# CS melayani customer pertama
+customer = antrian.popleft()
+print(f"Sedang melayani: {customer}")
+
+print("Antrian sekarang:", list(antrian))
